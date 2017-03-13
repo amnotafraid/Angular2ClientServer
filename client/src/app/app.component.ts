799536692a375146a031be05c22d071cc99e0bb5
amnotafraid <amnotafraid@gmail.com>
2017-03-13 13:11:02 -0500

    Change title

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-title = '@ngrx/store Async Pipe';
+title = '@ngrx/store Expanding State';
 }
